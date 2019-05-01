32f366de302e5ef2d57ce87acb008dc2a2ca6a6f
AndyBoWu <bwu2sfu@gmail.com>
2019-04-30 23:05:25 -0700

    Fixed all issues menitoned by Leo C, more testing needed for the new improvements

--- wallet_deployment/main.py
+++ wallet_deployment/main.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser
+
 import boto3
 import sys
 import json
 import yaml
-import pprint
+import argparse
 
 '''
 
 Usage: 	# $python3 main.py <key_ec2_tag> <value_ec2_tag> <wallet.sh>
+
+
+cmd for testing
+$python3 main.py -k tag:type -v testnode -d AWS-RunRemoteScript -s Github -o bwu2sfu -r harmony-ops -f aws/ssm -b branch:master -c nanny.sh -a aws2.json
 
 
 '''
@@ -17,32 +23,59 @@ Usage: 	# $python3 main.py <key_ec2_tag> <value_ec2_tag> <wallet.sh>
 
 def main():
 
-	with open("config-test-small.yaml", 'r') as stream:
-		try:
-			data = yaml.safe_load(stream)
-			key = data[0]['key']
-			values = data[0]['values']
-			document_name = data[0]['document_name']
-			source_type = data[0]['source_type']
-			owner = data[0]['owner']
-			repo = data[0]['repo']
-			path = data[0]['path']
-			get_options = data[0]['get_options']
-			command_line = data[0]['command_line']
-		except yaml.YAMLError as exc:
-			print(exc)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-p', '--profile', help = 'preset configuration files for different testing', required = False)
+	parser.add_argument('-k', '--key', help = 'key of the tag attached to the EC2 instance', required = False)
+	parser.add_argument('-v', '--value', help='value of the tag attached to the EC2 instance', required = False)
+	parser.add_argument('-d', '--document', help='document name', required = False)
+	parser.add_argument('-s', '--source', help='source type', required = False)
+	parser.add_argument('-o', '--owner', help='owner of the repo', required=False)
+	parser.add_argument('-r', '--repo', help='name of the repository', required=False)
+	parser.add_argument('-f', '--folder', help='path to the folder storing bash script', required=False)
+	parser.add_argument('-b', '--branch', help='branch', required=False)
+	parser.add_argument('-c', '--command', help='script to be executed', required=False)
+	parser.add_argument('-a', '--awsjson', help='json file to define aws regions', required=False)
 
-	# $python3 main.py <key_ec2_tag> <value_ec2_tag> <wallet.sh>
-	# Either this command takes all default values, or take values from command line
-	if len(sys.argv) > 1:
-		key = sys.argv[1]
-		values = sys.argv[2]
-		command_line = sys.argv[3]
+	args = vars(parser.parse_args())
+
+	# if the profile yaml file is given, parse the file
+	if args['profile']:
+		with open(args['profile'], 'r') as stream:
+			try:
+				data = yaml.safe_load(stream)
+				key = data[0]['key']
+				values = data[0]['values']
+				document_name = data[0]['document_name']
+				source_type = data[0]['source_type']
+				owner = data[0]['owner']
+				repo = data[0]['repo']
+				path = data[0]['path']
+				get_options = data[0]['get_options']
+				command_line = data[0]['command_line']
+			except yaml.YAMLError as exc:
+				print(exc)
+
+	else:
+		key = args['key']
+		values = args['value']
+		document_name = args['document']
+		source_type = args['source']
+		owner = args['owner']
+		repo = args['repo']
+		path = args['folder']
+		get_options = args['branch']
+		command_line = args['command']
 
 
 	# parse aws.json to fetch the names of all regions
+	aws_file = ''
+	if args['awsjson']:
+		aws_file = args['awsjson']
+	else:
+		aws_file = 'aws.json'
+
 	regions = []
-	with open('aws.json') as f:
+	with open(aws_file) as f:
 		aws_data = json.load(f)
 		for i in range(len(aws_data['regions'])):
 			regions.append(aws_data['regions'][i]['ext-name'])
@@ -50,6 +83,7 @@ def main():
 	for i in range(len(regions)):
 		client_name = 'client_' + regions[i]
 		client_name = boto3.client('ssm', region_name=regions[i])
+
 		response = client_name.send_command(Targets=[{"Key": key, "Values": [values, ]}, ], DocumentName=document_name, Parameters={"sourceType": [source_type], "sourceInfo": ["{\"owner\" : \"bwu2sfu\", \"repository\":\"harmony-ops\", \"path\":\"aws/ssm\", \"getOptions\":\"branch:master\"}"],"commandLine": [command_line]})
 
 
