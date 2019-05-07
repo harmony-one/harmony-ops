b9204868ab64a7788cbebf9ed9d3aeca781e6525
AndyBoWu <bwu2sfu@gmail.com>
2019-05-07 16:30:14 -0700

    added the curl cmd to test it out

--- aws/ssm/nanny.sh
+++ aws/ssm/nanny.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+
+curl -O https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/wallet.sh
+
 mkdir /home/ec2-user/$(date +%Y%m%d_%H%M%S)
+
+
 
 ps -ef | grep harmony
 
