90f6eee7e85f5dc07084cec31b59d3f1ad7486bd
AndyBoWu <bwu2sfu@gmail.com>
2019-05-07 16:26:56 -0700

    defined the path where to store the downloaded wallet

--- aws/ssm/deploy_wallet.sh
+++ aws/ssm/deploy_wallet.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+mkdir /home/ec2-user/$(date +%Y%m%d_%H%M%S)
+
+cd /home/ec2-user
+
 echo "downloading wallet.sh script .."
 curl -O https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/wallet.sh
 
