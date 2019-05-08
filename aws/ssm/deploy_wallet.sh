ec3afff1ab38268808bba82e3516310b249d5759
AndyBoWu <bwu2sfu@gmail.com>
2019-05-07 17:02:10 -0700

    remove the test folder creation cmd

--- aws/ssm/deploy_wallet.sh
+++ aws/ssm/deploy_wallet.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-
-mkdir /home/ec2-user/$(date +%Y%m%d_%H%M%S)
-
-cd /home/ec2-user
-
 echo "downloading wallet.sh script .."
 curl -O https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/wallet.sh
 
