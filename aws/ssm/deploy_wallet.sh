f716593d1f6c672dff90fe7edcb62eefa8ca55c5
AndyBoWu <bwu2sfu@gmail.com>
2019-05-07 17:03:50 -0700

    defined the path to store wallet

--- aws/ssm/deploy_wallet.sh
+++ aws/ssm/deploy_wallet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "downloading wallet.sh script .."
+
+cd /home/ec2-user
+
 curl -O https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/wallet.sh
 
-echo "chmoding wallet.sh .."
 chmod +x wallet.sh
 
-echo "launching wallet .."
 ./wallet.sh -d
