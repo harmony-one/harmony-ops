c63872fb27cb4cbed9a2408e9e0c8e197d43d69a
AndyBoWu <bwu2sfu@gmail.com>
2019-05-08 22:46:03 -0700

    Increased ERROR tolerance for ssm.send_command

--- wallet_deployment/main.py
+++ wallet_deployment/main.py
@@ -6,6 +6,8 @@ import json
 import yaml
 import argparse
 import ast
+import pprint
+import time
 '''
 
 Usage: 	# $python3 main.py <key_ec2_tag> <value_ec2_tag> <wallet.sh>
@@ -13,7 +15,7 @@ Usage: 	# $python3 main.py <key_ec2_tag> <value_ec2_tag> <wallet.sh>
 
 cmd for testing
 python3 main.py -k tag:type -v testnode -d AWS-RunRemoteScript -s GitHub -o bwu2sfu -r harmony-ops -f aws/ssm -b branch:master -c nanny.sh -a aws.json
-
+python3 main.py -k tag:Name -v 4-banjo-od-2019-04-30_22_14_16 -d AWS-RunRemoteScript -s GitHub -o bwu2sfu -r harmony-ops -f aws/ssm -b branch:master -c deploy_wallet.sh -a aws.json
 '''
 
 
@@ -79,67 +81,100 @@ def main():
 			regions.append(aws_data['regions'][i]['ext-name'])
 
 	for i in range(len(regions)):
-	# for i in range(1):
-		print("region name is: " + regions[i])
-		client_name = 'client_' + regions[i]
-		client_name = boto3.client('ssm', region_name=regions[i])
+	# for i in range(2):
+		
+		try:
+			print("----")
+			print("creating a SSM client in region: " + regions[i])
+			client_ssm_name = 'client_ssm_' + regions[i]
+			# print(client_ssm_name)
+			client_ssm_name = boto3.client('ssm', region_name=regions[i])
 
-		# print("key: " + key)
-		# print("values: " + values)
-		# print("source type: " + source_type)
-		# print("document name: " + document_name)
-		# print("owner: " + owner)
-		# print("repo: " + repo)
-		# print("path: " + path)
-		# print("get_options: " + get_options)
-		# print("command_line: " + command_line)
-		# print()
+			print("creating a EC2 client in region: " + regions[i])
+			client_ec2_name = 'client_ec2' + regions[i]
+			client_ec2_name = boto3.client('ec2', region_name=regions[i])
+			tag_filter = [{
+				'Name' : key,
+				'Values' : []
+			}]
+			tag_filter[0]["Values"].append(values)
 
+			ec2_resp_name = 'resp_ec2' + regions[i]
+			ec2_resp_name = client_ec2_name.describe_instances(Filters=tag_filter)
 
-		target_dict = {
-			"Key" : "",
-			"Values" : []
-		}
-		target_dict["Key"] = key
-		target_dict["Values"].append(values)
+			print("total number of test nodes: " + str(len(ec2_resp_name["Reservations"])))
 
-		target_array = []
-		target_array.append(target_dict)
-
-		# print("TARGET ARRAY")
-		# print(target_array)
-
-
-		# POC
-		para_dict = {
-			"sourceType" : [],
-			"sourceInfo" : [],
-			"commandLine" : []
-		}
+			# print("key: " + key)
+			# print("values: " + values)
+			# print("source type: " + source_type)
+			# print("document name: " + document_name)
+			# print("owner: " + owner)
+			# print("repo: " + repo)
+			# print("path: " + path)
+			# print("get_options: " + get_options)
+			# print("command_line: " + command_line)
+			# print()
 
 
-		para_dict["sourceType"].append(source_type)
-		para_dict["commandLine"].append(command_line)
+			target_dict = {
+				"Key" : "",
+				"Values" : []
+			}
+			target_dict["Key"] = key
+			target_dict["Values"].append(values)
 
-		info_json = {
-			"owner" : owner,
-			"repository" : repo,
-			"path" : path,
-			"getOptions" : get_options
+			target_array = []
+			target_array.append(target_dict)
 
-		}
+			# print("TARGET ARRAY")
+			# print(target_array)
 
-		para_dict["sourceInfo"].append(json.dumps(info_json))
 
-		# ref_string = '{"sourceType" : ["GitHub"],"sourceInfo" : ["{\"owner\" : \"bwu2sfu\", \"repository\":\"harmony-ops\", \"path\":\"aws/ssm\",\"getOptions\":\"branch:master\"}"],"commandLine" : ["nanny.sh"]}'
-		# print(ref_string)
+			# POC
+			para_dict = {
+				"sourceType" : [],
+				"sourceInfo" : [],
+				"commandLine" : []
+			}
 
-		# response = client_name.send_command(Targets=[{"Key": key, "Values": [values, ]}, ], DocumentName=document_name, Parameters={"sourceType": [source_type], "sourceInfo": ["{\"owner\" : \"bwu2sfu\", \"repository\":\"harmony-ops\", \"path\":\"aws/ssm\", \"getOptions\":\"branch:master\"}"],"commandLine": [command_line]})
-		# response = client_name.send_command(Targets = target_array, DocumentName = document_name, Parameters = ast.literal_eval(para_string))
 
-		# FUNCTIONAL CMD
-		# response = client_name.send_command(Targets=[{"Key": "tag:type", "Values": ["testnode", ]}, ], DocumentName="AWS-RunRemoteScript", Parameters={"sourceType": ["GitHub"], "sourceInfo": ["{\"owner\" : \"bwu2sfu\", \"repository\":\"harmony-ops\", \"path\":\"aws/ssm\", \"getOptions\":\"branch:master\"}"],"commandLine": ["nanny.sh"]})
-		response = client_name.send_command(Targets=target_array, DocumentName=document_name, Parameters=para_dict)
+			para_dict["sourceType"].append(source_type)
+			para_dict["commandLine"].append(command_line)
+
+			info_json = {
+				"owner" : owner,
+				"repository" : repo,
+				"path" : path,
+				"getOptions" : get_options
+
+			}
+
+			para_dict["sourceInfo"].append(json.dumps(info_json))
+
+			# ref_string = '{"sourceType" : ["GitHub"],"sourceInfo" : ["{\"owner\" : \"bwu2sfu\", \"repository\":\"harmony-ops\", \"path\":\"aws/ssm\",\"getOptions\":\"branch:master\"}"],"commandLine" : ["nanny.sh"]}'
+			# print(ref_string)
+
+			# response = client_name.send_command(Targets=[{"Key": key, "Values": [values, ]}, ], DocumentName=document_name, Parameters={"sourceType": [source_type], "sourceInfo": ["{\"owner\" : \"bwu2sfu\", \"repository\":\"harmony-ops\", \"path\":\"aws/ssm\", \"getOptions\":\"branch:master\"}"],"commandLine": [command_line]})
+			# response = client_name.send_command(Targets = target_array, DocumentName = document_name, Parameters = ast.literal_eval(para_string))
+
+			# FUNCTIONAL CMD
+			# response = client_name.send_command(Targets=[{"Key": "tag:type", "Values": ["testnode", ]}, ], DocumentName="AWS-RunRemoteScript", Parameters={"sourceType": ["GitHub"], "sourceInfo": ["{\"owner\" : \"bwu2sfu\", \"repository\":\"harmony-ops\", \"path\":\"aws/ssm\", \"getOptions\":\"branch:master\"}"],"commandLine": ["nanny.sh"]})
+			print("Invoking script stored in GitHub in region " + regions[i])
+			ssm_resp_name = 'resp_ssm' + regions[i]
+			# print(target_array)
+			# print(document_name)
+			# print(para_dict)
+			ssm_resp_name = client_ssm_name.send_command(Targets=target_array, DocumentName=document_name, Parameters=para_dict, MaxErrors='100%')
+			pprint.pprint(ssm_resp_name)
+
+
+			print("----")
+			time.sleep(1)
+		except Exception as e:
+			raise e 
+		else:
+			print('------------- succeded ---------------------')
+
 
 if __name__ == "__main__":
 	main()
