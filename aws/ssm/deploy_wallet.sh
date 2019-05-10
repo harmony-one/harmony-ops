955c5368d0fba10278b8cbdb13cfef47ff570e38
AndyBoWu <bwu2sfu@gmail.com>
2019-05-09 20:45:10 -0700

    starts with a clean env everytime for testing

--- aws/ssm/deploy_wallet.sh
+++ aws/ssm/deploy_wallet.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd /home/ec2-user
+rm -rf walletdemo
 
-cd /home/ec2-user/walletdemo
+mkdir walletdemo
 
 curl -O https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/wallet.sh
 
