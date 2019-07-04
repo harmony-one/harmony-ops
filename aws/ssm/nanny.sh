422e88f2126a13b3a8cce0b842f9e5cc5a764586
Andy Wu <bwu2sfu@gmail.com>
2019-07-03 22:39:22 -0700

    modified nanny.sh

--- aws/ssm/nanny.sh
+++ aws/ssm/nanny.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 
-curl -O https://raw.githubusercontent.com/harmony-one/harmony/master/scripts/wallet.sh
 
 mkdir /home/ec2-user/$(date +%Y%m%d_%H%M%S)
 
-
-
 ps -ef | grep harmony
 
-grep panic /home/ec2-user/soldier-*.log
