434365394f2b0c05098c0fa0899e1aaec814a97c
Leo Chen <leo@harmony.one>
2019-08-15 08:49:44 +0000

    [devop] add function to check hooray
    Signed-off-by: Leo Chen <leo@harmony.one>

--- devops/bin/devops.sh
+++ devops/bin/devops.sh
@@ -24,8 +24,9 @@ Actions:
    harmony           check the pid of the harmony process
    start_soldier     start the soldier process
    start_harmony     reinit/start the harmony process
-   log               print the last 10 lines of validator-*.log
-   bingo             grep the BINGO from the last 100 lines of validator-*.log
+   log               print the last 10/x lines of validator-*.log
+   hooray            grep the HOORAY from the last 1000 lines of validator-*.log
+   bingo             grep the BINGO from the last 1000 lines of validator-*.log
    workflow_1        workflow of checking and restarting soldier/harmony process (DEFAULT)
 
 Examples:
@@ -34,6 +35,8 @@ Examples:
 # one IP address per line
 
    $ME -a uptime host-1.txt
+
+   $ME -a log host-2.txt 100
 
 EOT
    exit 0
@@ -98,12 +101,13 @@ function start_harmony
 
 function log
 {
+   num=${1:-10}
    echo ============================
-   read -p "checking last 10 lines of logs (y/n)?" yn
+   read -p "checking last $num lines of logs (y/n)?" yn
    if [[ "$yn" == "y" || "$yn" == "Y" ]]; then
       for i in $(cat $HOST); do
          echo "********** checking $i ***********";
-         ${SSH} -p $PROFILE ec2-user@$i 'tail -n 10 ../tmp_log/*/validator*.log ';
+         ${SSH} -p $PROFILE ec2-user@$i "tail -n $num ../tmp_log/*/validator*.log ";
          echo;
       done
    fi
@@ -118,7 +122,22 @@ function bingo
       sleep 30
       for i in $(cat $HOST); do
          echo "********** checking $i ***********";
-         ${SSH} -p $PROFILE ec2-user@$i 'tail -n 100 ../tmp_log/*/validator*.log | grep BINGO ';
+         ${SSH} -p $PROFILE ec2-user@$i 'tac ../tmp_log/*/validator*.log | grep -m 1 BINGO ';
+         echo;
+      done
+   fi
+}
+
+function hooray
+{
+   echo ============================
+   read -p "checking HOORAY (y/n)?" yn
+   if [[ "$yn" == "y" || "$yn" == "Y" ]]; then
+      echo sleeping 30 sec ...
+      sleep 30
+      for i in $(cat $HOST); do
+         echo "********** checking $i ***********";
+         ${SSH} -p $PROFILE ec2-user@$i 'tac ../tmp_log/*/validator*.log | grep -m 1 HOORAY';
          echo;
       done
    fi
@@ -164,6 +183,7 @@ done
 shift $(($OPTIND - 1))
 
 HOST=$1
+shift
 
 if [ -z "$HOST" ]; then
    usage
@@ -177,8 +197,9 @@ case $ACTION in
    harmony) harmony ;;
    start_soldier) start_soldier ;;
    start_harmony) start_harmony ;;
-   log) log ;;
+   log) log $*;;
    bingo) bingo ;;
+   hooray) hooray ;;
    workflow_1) do_workflow_one ;;
 esac
 
