6fcd41d4243ff1f3c91985ad52068fe8cc30a054
Edgar Aroutiounian <edgar.factorial@gmail.com>
2019-09-20 20:50:25 -0700

    [harmony-watchdog] First cut of correct impl

--- monitoring/daemons/harmony-monitor/node-metadata-watcher/reporting-server.go
+++ monitoring/daemons/harmony-monitor/node-metadata-watcher/reporting-server.go
@@ -1,52 +1,26 @@
 package main
 
 import (
+	"bytes"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"reflect"
+	"strconv"
+	"time"
 )
 
 const (
-	reportTemplate = `
-<!DOCTYPE html>
-<html lang="en">
-  <meta charset="utf-8" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0" name="viewport">
-  <head>
-<style>
-* {margin:0;padding:0;}
-
- .CenteredReport {
-   display:flex;
- }
-
- .NodeDetail {
-  background-color:aliceblue;
-  display:flex;
-  padding: 10px;
-}
-</style>
-
-  </head>
-  <body>
-<div class="CenteredReport">
-  <header>{{.Title}}</header>
-  <p>First report</p>
-
-{{range .Nodes}}
-<div class="NodeDetail">
-  <p>BLS public key: {{.BLSPublicKey}} </p>
-  <p>Version: {{.Version}} </p>
-  <p>NetworkType: {{.NetworkType}} </p>
-  <p>ChainID: {{.ChainID}} </p>
-</div>
-{{end}}
-</div>
-
-
-  </body>
-</html>
-`
+	versionJSONRPC     = "2.0"
+	blocksPerEpoch     = 16384
+	blockTime          = time.Second * 15 // Come back to this
+	metadataRPC        = "hmy_getNodeMetadata"
+	blockHeaderRPC     = "hmy_latestHeader"
+	blockHeaderReport  = "block-header"
+	nodeMetadataReport = "node-metadata"
 )
 
 type nodeMetadata struct {
@@ -56,28 +30,197 @@ type nodeMetadata struct {
 	ChainID      string `json:"chainid"`
 }
 
-type report struct {
-	Title string
-	Nodes []nodeMetadata
+type headerInformation struct {
+	BlockHash        string `json:"blockHash"`
+	BlockNumber      uint64 `json:"blockNumber"`
+	ShardID          uint32 `json:"shardID"`
+	Leader           string `json:"leader"`
+	ViewID           uint64 `json:"viewID"`
+	Epoch            uint64 `json:"epoch"`
+	Timestamp        string `json:"timestamp"`
+	UnixTime         uint64 `json:"unixtime"`
+	LastCommitSig    string `json:"lastCommitSig"`
+	LastCommitBitmap string `json:"lastCommitBitmap"`
 }
 
-func createReport(title string) report {
+var (
+	queryID                    = 0
+	nodeMetadataCSVHeader      []string
+	headerInformationCSVHeader []string
+)
+
+func init() {
+	h := reflect.TypeOf((*headerInformation)(nil)).Elem()
+	n := reflect.TypeOf((*nodeMetadata)(nil)).Elem()
+	for i := 0; i < h.NumField(); i++ {
+		headerInformationCSVHeader = append(headerInformationCSVHeader, h.Field(i).Name)
+	}
+	for i := 0; i < n.NumField(); i++ {
+		nodeMetadataCSVHeader = append(nodeMetadataCSVHeader, n.Field(i).Name)
+	}
+}
+
+func request(node, rpcMethod string) ([]byte, error) {
+	requestBody, _ := json.Marshal(map[string]interface{}{
+		"jsonrpc": versionJSONRPC,
+		"id":      strconv.Itoa(queryID),
+		"method":  rpcMethod,
+		"params":  []interface{}{},
+	})
+	resp, err := http.Post(node, "application/json", bytes.NewBuffer(requestBody))
+	fmt.Printf("URL: <%s>, Request Body: %s\n\n", node, string(requestBody))
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	queryID++
+	fmt.Printf("URL: %s, Response Body: %s\n\n", node, string(body))
+	return body, nil
+}
+
+func (m *monitor) renderReport(w http.ResponseWriter, req *http.Request) {
+	t, e := template.New("report").Parse(reportPage())
+	fmt.Println(t, e)
+	type v struct {
+		Title         []string
+		NodesMetadata []metadataRPCResult
+		NodesHeader   []headerInfoRPCResult
+	}
+	for i, n := range m.BlockHeaderSnapshot.Nodes {
+		shorted := n.Payload.LastCommitSig[:5] + "..." + n.Payload.LastCommitSig[len(n.Payload.LastCommitSig)-5:]
+		m.BlockHeaderSnapshot.Nodes[i].Payload.LastCommitSig = shorted
+	}
+
+	t.Execute(w, v{
+		Title:         []string{m.chain, time.Now().Format(time.RFC3339), versionS()},
+		NodesMetadata: m.MetadataSnapshot.Nodes,
+		NodesHeader:   m.BlockHeaderSnapshot.Nodes,
+	})
+}
+
+func (m *monitor) produceCSV(w http.ResponseWriter, req *http.Request) {
+	filename := ""
+	records := [][]string{}
+	switch keys, ok := req.URL.Query()["report"]; ok {
+	case true:
+		switch report := keys[0]; report {
+		case blockHeaderReport:
+			filename = blockHeaderReport + ".csv"
+			records = append(records, headerInformationCSVHeader)
+		case nodeMetadataReport:
+			filename = nodeMetadataReport + ".csv"
+			records = append(records, nodeMetadataCSVHeader)
+		}
+	default:
+		http.Error(w, "report not chosen in query param", http.StatusBadRequest)
+		return
+	}
+	fmt.Println(req.URL.Query())
+	fmt.Println(records)
+	w.Header().Set("Content-Type", "text/csv")
+	w.Header().Set("Content-Disposition", "attachment;filename="+filename)
+	wr := csv.NewWriter(w)
+	// records := [][]string{
+	// 	{"first_name", "last_name", "username"},
+	// 	{"Rob", "Pike", "rob"},
+	// 	{"Ken", "Thompson", "ken"},
+	// 	{"Robert", "Griesemer", "gri"},
+	// }
+	err := wr.WriteAll(records)
+	if err != nil {
+		http.Error(w, "Error sending csv: "+err.Error(), http.StatusInternalServerError)
+	}
+}
+
+type metadataRPCResult struct {
+	Payload nodeMetadata
+	IP      string
+}
+
+type headerInfoRPCResult struct {
+	Payload headerInformation
+	IP      string
+}
+
+type monitor struct {
+	chain            string
+	MetadataSnapshot struct {
+		TS    time.Time
+		Nodes []metadataRPCResult
+	}
+	BlockHeaderSnapshot struct {
+		TS    time.Time
+		Nodes []headerInfoRPCResult
+	}
+}
+
+func (m *monitor) update(rpc string, every int, nodeList []string) {
 	type t struct {
+		addr      string
+		rpcResult []byte
+		oops      error
+	}
+	for now := range time.Tick(time.Duration(every) * time.Second) {
+		// Reset
+		m.MetadataSnapshot.Nodes = []metadataRPCResult{}
+		m.BlockHeaderSnapshot.Nodes = []headerInfoRPCResult{}
+		go func(n time.Time) {
+			payloadChan := make(chan t, len(nodeList))
+			for _, nodeIP := range nodeList {
+				go func(addr string) {
+					result := t{addr: addr}
+					result.rpcResult, result.oops = request("http://"+addr, rpc)
+					payloadChan <- result
+				}(nodeIP)
+			}
+			for range nodeList {
+				it := <-payloadChan
+				if it.oops != nil {
+					fmt.Println("Some log of oops", it.oops)
+					continue
+				}
+				m.bytesToNodeMetadata(rpc, it.addr, it.rpcResult)
+			}
+			m.MetadataSnapshot.TS = n
+		}(now)
+	}
+}
+
+func (m *monitor) bytesToNodeMetadata(rpc, addr string, payload []byte) {
+	type r struct {
 		Result nodeMetadata `json:"result"`
 	}
-	nodeReport := baseRequest("http://localhost:9500")
-	oneReport := t{}
-	json.Unmarshal(nodeReport, &oneReport)
-	fmt.Println(oneReport.Result)
-	return report{"some-report-example", []nodeMetadata{oneReport.Result}}
+	type s struct {
+		Result headerInformation `json:"result"`
+	}
+	switch rpc {
+	case metadataRPC:
+		oneReport := r{}
+		json.Unmarshal(payload, &oneReport)
+		m.MetadataSnapshot.Nodes = append(m.MetadataSnapshot.Nodes, metadataRPCResult{
+			oneReport.Result,
+			addr,
+		})
+	case blockHeaderRPC:
+		oneReport := s{}
+		json.Unmarshal(payload, &oneReport)
+		m.BlockHeaderSnapshot.Nodes = append(m.BlockHeaderSnapshot.Nodes, headerInfoRPCResult{
+			oneReport.Result,
+			addr,
+		})
+	}
 }
 
-func renderReport(w http.ResponseWriter, req *http.Request) {
-	t, _ := template.New("report").Parse(reportTemplate)
-	t.Execute(w, createReport("SHOULD COME FROM YAML CONFIG"))
-}
+func (m *monitor) startReportingHTTPServer(instrs *instruction) {
+	go m.update(metadataRPC, instrs.InspectSchedule.NodeMetadata, instrs.networkNodes)
+	go m.update(blockHeaderRPC, instrs.InspectSchedule.BlockHeader, instrs.networkNodes)
 
-func startReportingHTTPServer() {
-	http.HandleFunc("/report", renderReport)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/report-table", m.renderReport)
+	http.HandleFunc("/report-download", m.produceCSV)
+	http.ListenAndServe(":"+strconv.Itoa(instrs.HTTPReporter.Port), nil)
 }
--- monitoring/daemons/harmony-monitor/node-metadata-watcher/root.go
+++ monitoring/daemons/harmony-monitor/node-metadata-watcher/root.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
@@ -14,6 +15,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/takama/daemon"
+	"gopkg.in/yaml.v2"
 )
 
 const (
@@ -22,15 +24,29 @@ const (
 	port        = ":9977"
 )
 
-//    dependencies that are NOT required by the service, but might be used
 var (
-	stdlog, errlog *log.Logger
-	dependencies   = []string{"dummy.service"}
+	sep       = []byte("\n")
+	recordSep = []byte(" ")
+	rootCmd   = &cobra.Command{
+		Use:          "harmony-watchdog",
+		SilenceUsage: true,
+		Long:         "Monitor a blockchain",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+	monitorNodeYAML string
+	stdlog          *log.Logger
+	errlog          *log.Logger
+	//    dependencies that are NOT required by the service, but might be used
+	dependencies = []string{"dummy.service"}
 )
 
 // Service has embedded daemon
 type Service struct {
 	daemon.Daemon
+	*monitor
+	*instruction
 }
 
 // Manage by daemon commands or run the daemon
@@ -46,7 +62,6 @@ func (service *Service) Manage() error {
 	// if we're not ready to receive when the signal is sent.
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, os.Kill, syscall.SIGTERM)
-
 	// Set up listener for defined host and port
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
@@ -54,13 +69,14 @@ func (service *Service) Manage() error {
 	}
 	// set up channel on which to send accepted connections
 	listen := make(chan net.Conn, 100)
+	go service.startReportingHTTPServer(service.instruction)
 	go acceptConnection(listener, listen)
 	// loop work cycle with accept connections or interrupt
 	// by system signal
 	for {
 		select {
 		case conn := <-listen:
-			go handleClient(conn)
+			go handleClient(conn, service.instruction.Auth.PagerDuty.EventServiceKey)
 		case killSignal := <-interrupt:
 			stdlog.Println("Got signal:", killSignal)
 			stdlog.Println("Stoping listening on ", listener.Addr())
@@ -85,7 +101,7 @@ func acceptConnection(listener net.Listener, listen chan<- net.Conn) {
 	}
 }
 
-func handleClient(client net.Conn) {
+func handleClient(client net.Conn, serviceKey string) {
 	for {
 		buf := make([]byte, 4096)
 		numbytes, err := client.Read(buf)
@@ -96,11 +112,77 @@ func handleClient(client net.Conn) {
 		fmt.Println(cmd)
 		switch cmd {
 		case "work":
-			client.Write(baseRequest("http://localhost:9500"))
+			// client.Write(baseRequest("http://localhost:9500"))
+		case "pager":
+			notifyTeam(serviceKey)
 		default:
 			client.Write(buf[:numbytes])
 		}
 	}
+}
+
+type watchParams struct {
+	Auth struct {
+		PagerDuty struct {
+			EventServiceKey string `yaml:"event-service-key"`
+		} `yaml:"pagerduty"`
+	} `yaml:"auth"`
+	TargetChain string `yaml:"target-chain"`
+	// Assumes Seconds
+	InspectSchedule struct {
+		BlockHeader  int `yaml:"block-header"`
+		NodeMetadata int `yaml:"node-metadata"`
+	} `yaml:"inspect-schedule"`
+	HTTPReporter struct {
+		Port int `yaml:"port"`
+	} `yaml:"http-reporter"`
+	ReportWhen struct {
+		BlockDeviation int `yaml:"block-deviation"`
+		NodeThreshold  int `yaml:"threshold"`
+	} `yaml:"report-when"`
+	DistributionFiles struct {
+		ConfigTxt string `yaml:"config-txt"`
+		RawIPTxt  string `yaml:"raw-ip-txt"`
+	} `yaml:"node-distribution"`
+}
+
+type instruction struct {
+	watchParams
+	networkNodes []string
+}
+
+func newInstructions(yamlPath string) (*instruction, error) {
+	rawYAML, err := ioutil.ReadFile(yamlPath)
+	if err != nil {
+		return nil, err
+	}
+	t := watchParams{}
+	err = yaml.Unmarshal(rawYAML, &t)
+	if err != nil {
+		return nil, err
+	}
+	nodesRecord, err2 := ioutil.ReadFile(t.DistributionFiles.ConfigTxt)
+	if err2 != nil {
+		return nil, err2
+	}
+	networkNodes := bytes.Split(bytes.Trim(nodesRecord, "\n"), sep)
+	instr := &instruction{t, []string{}}
+	for _, value := range networkNodes {
+		instr.networkNodes = append(
+			instr.networkNodes,
+			// Trust the input because it is already trusted elsewhere,
+			// if data malformed, then launch would have failed anyway
+			strings.TrimSpace(string(bytes.Split(value, recordSep)[0]))+":9500",
+		)
+	}
+	return instr, nil
+}
+
+func versionS() string {
+	return fmt.Sprintf(
+		"Harmony (C) 2019. %v, version %v-%v (%v %v)",
+		path.Base(os.Args[0]), version, commit, builtBy, builtAt,
+	)
 }
 
 func init() {
@@ -110,38 +192,36 @@ func init() {
 		Use:   "version",
 		Short: "Show version",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Fprintf(os.Stderr,
-				"Harmony (C) 2019. %v, version %v-%v (%v %v)\n",
-				path.Base(os.Args[0]), version, commit, builtBy, builtAt)
+			fmt.Fprintf(os.Stderr, versionS()+"\n")
 			os.Exit(0)
 		},
 	})
-	rootCmd.AddCommand(&cobra.Command{
+
+	monitor := &cobra.Command{
 		Use:   "monitor",
 		Short: "watch the blockchain for problems",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			instr, err := newInstructions(monitorNodeYAML)
+			if err != nil {
+				return err
+			}
 			srv, err := daemon.New(name, description, dependencies...)
 			if err != nil {
 				return err
 			}
-			service := &Service{srv}
+			m := &monitor{chain: instr.TargetChain}
+			service := &Service{srv, m, instr}
 			err = service.Manage()
 			if err != nil {
 				return err
 			}
 			return nil
-
-		},
-	})
-}
-
-var (
-	rootCmd = &cobra.Command{
-		Use:          "harmony-watchdog",
-		SilenceUsage: true,
-		Long:         "Monitor a blockchain",
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
 		},
 	}
-)
+
+	rootCmd.AddCommand(monitor)
+	descr := "yaml detailing what to watch"
+	monitor.Flags().StringVar(&monitorNodeYAML, "watch", "", descr)
+	monitor.MarkFlagRequired("watch")
+
+}
