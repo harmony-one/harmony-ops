54a1bacf0fd311892155e3d7cf54299b69cc312a
Andy Wu <andy@harmony.one>
2019-09-21 12:25:57 -0700

    hide domain ip, add min storage space alert

--- grafana-dashboard/main.py
+++ grafana-dashboard/main.py
@@ -6,7 +6,9 @@ import requests
 import json
 import pprint
 import copy
-# from botocore.exceptions import ClientError
+from dotenv import load_dotenv
+
+load_dotenv()
 
 bucket_log = r'harmony-benchmark'
 
@@ -23,11 +25,13 @@ f_array = [path_shard0_tf,
            path_shard2_tf,
            path_shard3_tf]
 
+
 API_SECRET = os.getenv("SECRET_KEY")
+Grafana_IP = os.getenv("GRAFANA_IP")
 
 headers = {"Content-Type": 'application/json'}
 URL_update_db = 'http://api_key:' + API_SECRET + \
-    '@18.144.50.152:3000/api/dashboards/db'
+    '@' + Grafana_IP + ':3000/api/dashboards/db'
 
 shard0_ip_array = []
 shard1_ip_array = []
@@ -48,6 +52,8 @@ dict_db_shard = {
     3: "db_shard3.json"
 }
 
+# min_storage_space for alerting
+min_storage_space = 5
 
 def download_ip_list_from_s3(files_array):
 
@@ -81,6 +87,7 @@ def add_cpu_ram_disk_charts(dict_ip_array, dict_shard, dict_shard_json_db_offlin
     for ind in range(4):
         ip_array = dict_ip_array.get(ind)
         ip_array_size = len(ip_array)
+        # ip_array_size = 2
 
         shard = dict_shard.get(ind)
 
@@ -98,14 +105,20 @@ def add_cpu_ram_disk_charts(dict_ip_array, dict_shard, dict_shard_json_db_offlin
                 x_point = idy * 6
                 y_point = (idx+1) * 6
 
-                cpu_query = "100 - (avg by (instance) (irate(node_cpu_seconds_total{instance=\""+ip+':9100\",job=\"'+shard+'\",mode=\"idle\"}[5m])) * 100)'
-                ram_query = "(node_memory_MemTotal_bytes{instance=\""+ip+':9100\",job=\"'+shard+'\"} - node_memory_MemFree_bytes{instance=\"'+ip.rstrip()+':9100\",job=\"'+shard+'\"}) / node_memory_MemTotal_bytes{instance=\"'+ip.rstrip()+':9100\",job=\"'+shard+'\"} * 100'
-                memory_actual_usage_query = "(node_memory_MemTotal_bytes{instance=\""+ip+":9100\"} - node_memory_MemAvailable_bytes{instance=\""+ip+":9100\"}) * 100/ node_memory_MemTotal_bytes{instance=\""+ip+":9100\"}"
-                disk_query = "node_filesystem_avail_bytes{instance=\""+ip+':9100\",job=\"'+shard+'\", mountpoint="/"}/1024/1024/1024'
-                network_incoming_query = "rate(node_network_receive_bytes_total{instance=\""+ip+":9100\", device=\"eth0\"}[5m]) / 1024"
-                network_outgoing_query = "rate(node_network_transmit_bytes_total{instance=\""+ip+":9100\", device=\"eth0\"}[5m]) / 1024"
+                cpu_query = "100 - (avg by (instance) (irate(node_cpu_seconds_total{instance=\"" + \
+                    ip+':9100\",job=\"'+shard+'\",mode=\"idle\"}[5m])) * 100)'
+                ram_query = "(node_memory_MemTotal_bytes{instance=\""+ip+':9100\",job=\"'+shard+'\"} - node_memory_MemFree_bytes{instance=\"'+ip.rstrip(
+                )+':9100\",job=\"'+shard+'\"}) / node_memory_MemTotal_bytes{instance=\"'+ip.rstrip()+':9100\",job=\"'+shard+'\"} * 100'
+                memory_actual_usage_query = "(node_memory_MemTotal_bytes{instance=\""+ip+":9100\"} - node_memory_MemAvailable_bytes{instance=\"" + \
+                    ip+":9100\"}) * 100/ node_memory_MemTotal_bytes{instance=\""+ip+":9100\"}"
+                disk_query = "node_filesystem_avail_bytes{instance=\""+ip + \
+                    ':9100\",job=\"'+shard+'\", mountpoint="/"}/1024/1024/1024'
+                network_incoming_query = "rate(node_network_receive_bytes_total{instance=\"" + \
+                    ip+":9100\", device=\"eth0\"}[5m]) / 1024"
+                network_outgoing_query = "rate(node_network_transmit_bytes_total{instance=\"" + \
+                    ip+":9100\", device=\"eth0\"}[5m]) / 1024"
 
-                ## start to customize the chart
+                # start to customize the chart
                 data_cpu_insert = copy.deepcopy(cpu_temp_json)
 
                 temp_expr_2 = {
@@ -120,26 +133,71 @@ def add_cpu_ram_disk_charts(dict_ip_array, dict_shard, dict_shard_json_db_offlin
                 # FIRST COLUMN - CPU Monitoring
                 if (x_point == 0):
                     title_chart = "CPU UTILIZATION - "
-                    data_cpu_insert["targets"][0].update({"expr" : cpu_query})
+                    data_cpu_insert["targets"][0].update({"expr": cpu_query})
                     data_cpu_insert["yaxes"][0].update({"label": "%"})
                 # SECOND COLUMN - RAM Monitoring
                 elif (x_point == 6):
                     title_chart = "MEMORY - "
-                    data_cpu_insert["targets"][0].update({"expr" : ram_query})
-                    data_cpu_insert["targets"][0].update({"legendFormat" : "Occupied Memory"})
+                    data_cpu_insert["targets"][0].update({"expr": ram_query})
+                    data_cpu_insert["targets"][0].update(
+                        {"legendFormat": "Occupied Memory"})
                     data_cpu_insert["yaxes"][0].update({"label": "%"})
                     data_cpu_insert["targets"].append(temp_expr_2.copy())
                     data_cpu_insert["targets"][1].update({
-                                                "expr" : memory_actual_usage_query,
-                                                "legendFormat" : "Actual Usage",
-                                                "refId" : "B"
+                        "expr": memory_actual_usage_query,
+                        "legendFormat": "Actual Usage",
+                        "refId": "B"
                     })
 
                 # THIRD COLUMN - DISK Monitoring
                 elif (x_point == 12):
+                    # add an alert for low storage
+                    alert_json = {
+                        "alertRuleTags": {},
+                        "conditions": [
+                            {
+                                "evaluator": {
+                                    "params": [
+                                        min_storage_space
+                                    ],
+                                    "type": "lt"
+                                },
+                                "operator": {
+                                    "type": "and"
+                                },
+                                "query": {
+                                    "params": [
+                                        "A",
+                                        "5m",
+                                        "now"
+                                    ]
+                                },
+                                "reducer": {
+                                    "params": [],
+                                    "type": "avg"
+                                },
+                                "type": "query"
+                            }
+                        ],
+                        "for": "5m",
+                        "frequency": "1m",
+                        "handler": 1,
+                        "message": "Run out of storage space",
+                        "name": "Node is running out of storage space",
+                        "noDataState": "no_data",
+                        "notifications": [
+                            {
+                                "uid": "2yKOXNpWk"
+                            }
+                        ]
+                    }
+
                     title_chart = "FREE SPACE -"
-                    data_cpu_insert["targets"][0].update({"expr" : disk_query})
+                    data_cpu_insert["targets"][0].update({"expr": disk_query})
                     data_cpu_insert["yaxes"][0].update({"label": "GB"})
+                    # data_cpu_insert.update({"alert" : alert_json})
+
+
                 elif (x_point == 18):
                     title_chart = "NETWORK TRAFFIC - "
                     temp_expr = {
@@ -153,24 +211,21 @@ def add_cpu_ram_disk_charts(dict_ip_array, dict_shard, dict_shard_json_db_offlin
                     data_cpu_insert["targets"].append(temp_expr.copy())
                     # update outgoing traffic
                     data_cpu_insert["targets"][1].update({
-                                                "expr" : network_outgoing_query,
-                                                "legendFormat" : "OUTGOING",
-                                                "refId" : "B"
+                        "expr": network_outgoing_query,
+                        "legendFormat": "OUTGOING",
+                        "refId": "B"
                     })
-                    data_cpu_insert["yaxes"][0].update({"label" : "KB"})
+                    data_cpu_insert["yaxes"][0].update({"label": "KB"})
                 else:
                     pass
 
-
-                data_cpu_insert.update({"gridPos" : {'h' : 6, 'w' : 6, 'x' : x_point, 'y' : y_point},
-                                      "id" : id_0,
-                                      "title": title_chart + ip
-                                      })
+                data_cpu_insert.update({"gridPos": {'h': 6, 'w': 6, 'x': x_point, 'y': y_point},
+                                        "id": id_0,
+                                        "title": title_chart + ip
+                                        })
 
                 # insert this chart to dashboard.panels
                 data_db_shard["dashboard"]["panels"].append(data_cpu_insert)
-
-
 
         with open(db_shard, 'w') as fp:
             json.dump(data_db_shard, fp)
