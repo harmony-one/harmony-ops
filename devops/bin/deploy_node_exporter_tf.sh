8b22797363b1e8b4424312fdbe89c29ae1225c36
Andy Wu <andy@harmony.one>
2019-09-21 14:12:22 -0700

    update node exporter version fixed a typo add functionality to remove install file and unzipped folder

--- devops/bin/deploy_node_exporter_tf.sh
+++ devops/bin/deploy_node_exporter_tf.sh
@@ -1,36 +1,41 @@
-set -eu
-OS=$(uname -s)
-os=$(echo "$OS" | awk '{print tolower($0)}')
+#!/usr/bin/env bash
 
-# node_exporter version: 0.18.0/2019-05-09
-URL_node_exporter_linux=https://github.com/prometheus/node_exporter/releases/download/v0.18.0/node_exporter-0.18.0.linux-amd64.tar.gz
+# latest node_exporter version: 0.18.1/2019-06-04
+URL_node_exporter_linux=https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
 
 # download and decompress the node exporter in the tmp folder
+echo "downloading node_exporter ..."
 curl -LO $URL_node_exporter_linux
-tar -xvf /tmp/node_exporter-0.18.0.$os-amd64.tar.gz
-# add a servcie account for node_exporter
-useradd -rs /bin/false node_exporter
 
-# move the node export binary to /usr/local/bin
-mv -f /tmp/node_exporter-0.18.0.$os-amd64/node_exporter /usr/local/bin/
+
+echo "unzipping the file ..."
+tar -xvf node_exporter-*.tar.gz
+echo "moving node_exporter to the proper place ..."
+mv -f node_exporter-*-amd64/node_exporter /usr/local/bin/
+echo "cleaning up ..."
+rm node_exporter-*.tar.gz
+rm -r node_exporter-*-amd64/
+
+# add a servcie account for node_exporter
+echo "adding user node_exporter ..."
+id -u node_exporter &>/dev/null || sudo useradd -rs /bin/false node_exporter
 
 # create a node_exporter service file under systemd
-node_exporter_service=/etc/systemd/system/node_exporter.service # Linux only
+node_exporter_service=/etc/systemd/system/node_exporter.service
+echo "creating node exporter service file ..."
 echo "[Unit]
    Description=Node Exporter
    After=network.target
-
    [Service]
    User=node_exporter
    Group=node_exporter
    Type=simple
    ExecStart=/usr/local/bin/node_exporter
-
    [Install]
    WantedBy=multi-user.target" >$node_exporter_service
 
-systemctl daemon-reload
-systemctl start node_exporter
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
 
 #enable the node exporter servie to the system startup
-systemctl enable node_exporte
+sudo systemctl enable node_exporter
