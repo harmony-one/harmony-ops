ea457bbe700223d466bdb068de7c853a87b8fe41
Leo Chen <leo@harmony.one>
2019-10-23 15:03:06 -0700

    [1h] add retry
    Signed-off-by: Leo Chen <leo@harmony.one>

--- monitoring/balance-monitoring/check.sh
+++ monitoring/balance-monitoring/check.sh
@@ -20,17 +20,41 @@ if [[ -f "captures/$hour/$minute/$FILE" ]]; then
     touch captures/$hour/$minute/$FILE
 fi
 
+function get_balance
+{
+   address=$1
+   shard=$2
+
+   local result=$(LD_LIBRARY_PATH=wallet_files wallet_files/hmy balance -n http://s${shard}.t.hmny.io:9500 "$address" | jq '.[].amount' | tr "\n" + | sed s/+$//)
+   local amount=$(echo "$result" | bc -l)
+
+   echo $amount
+}
+
 while read add; do
    address=$(echo "$add" | cut -d " " -f 1)
    shard=$(echo "$add" | cut -d " " -f 2)
-   result=$(LD_LIBRARY_PATH=wallet_files wallet_files/hmy balance -n http://s${shard}.t.hmny.io:9500 "$address" | jq '.[].amount' | tr "\n" + | sed s/+$//)
-   amount=$(echo "$result" | bc -l)
-   if [ -n "$amount" ]; then
+   retry=0
+   succeed=false
+
+   while [ $retry -lt 3 ]; do
+      amount=$(get_balance $address $shard)
+      if [ -n "$amount" ]; then
+         succeed=true
+         break
+      fi
+      sleep 1
+      (( retry ++ ))
+   done
+
+   if [ "$succeed" == "true" ]; then
       echo "$address $shard $amount" >> captures/$hour/$minute/$FILE
    else
+      echo $address/$shard check balance failed > /dev/stderr
       echo "$address -1 0" >> captures/$hour/$minute/$FILE
    fi
-   sleep 1
+
+   sleep 0.1
 done < $ADDR
 
 rm $ADDR
