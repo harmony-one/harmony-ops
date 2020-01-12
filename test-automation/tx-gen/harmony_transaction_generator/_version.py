5ea55113ee59f3ec294de95fd40508edd053e370
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-01-11 19:52:36 -0800

    [tx-gen] Update version

--- test-automation/tx-gen/harmony_transaction_generator/_version.py
+++ test-automation/tx-gen/harmony_transaction_generator/_version.py
@@ -7,5 +7,5 @@ Provides harmony_transaction_generator version information.
 
 from incremental import Version
 
-__version__ = Version('harmony_transaction_generator', 20, 1, 5)
+__version__ = Version('harmony_transaction_generator', 20, 1, 7)
 __all__ = ["__version__"]
