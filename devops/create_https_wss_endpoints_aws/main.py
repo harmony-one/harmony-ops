5f35f50890544e792306497d20137c010cd77fcf
Andy Wu <andy@harmony.one>
2020-01-14 19:34:39 -0800

    add tg config to testnet.json

--- devops/create_https_wss_endpoints_aws/main.py
+++ devops/create_https_wss_endpoints_aws/main.py
@@ -49,21 +49,64 @@ def parse_network_config(param):
 
 
 
-print(parse_network_config("explorers"))
+# print(parse_network_config("explorers"))
 
 def create_endpoints_new_network():
     pass
 
 
-def retrieve_instance_id(array_instance_ip):
-    """ mapping from instance ip -> instance-id """
-    pass
+def shcmd2(cmd, ignore_error=False):
+    """ customized version of shcmd created by aw """
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    output = proc.stdout.read()
+    output_string = output.decode("utf-8")
+    return output_string
 
+def retrieve_instance_region(ip):
+    """ deduce instance region from its ipv4 """
+    cmd = "host {ip}".format(ip=ip)
+    resp = shcmd2(cmd)
+    info = resp.split('.')
+    if info[-4] ==  'compute':
+        region = info[-5]
+    elif info[-4] == 'compute-1':
+        region = 'us-east-1'
+    else:
+        raise ValueError("cannot deduce region from name {}".format(info))
+    return region
+
+
+def retrieve_instance_id(array_instance_ip):
+    """ mapping from instance-ip -> instance-id """
+    array_instance_id = []
+    for ip in array_instance_ip:
+        region = retrieve_instance_region(ip)
+        ec2_client = boto3.client('ec2', region_name=region)
+        response = ec2_client.describe_instances(Filters=[{'Name': 'ip-address', 'Values': [ip]}])
+        instance_id = response["Reservations"][0]["Instances"][0]["InstanceId"]
+        array_instance_id.append(instance_id)
+
+    return array_instance_id
 
 def update_target_groups():
     """  """
-    array_instance_ip = parse_network_config("explorers")
-    array_instance_id = retrieve_instance_id(array_instance_ip)
+
+    num_of_shards = parse_network_config("num_of_shards")
+
+    # add instance_id into an array for each shard
+    for i in range(num_of_shards):
+        key_explorer = "explorers_" + str(i)
+        array_instance_ip = parse_network_config(key_explorer)
+        array_instance_id = retrieve_instance_id(array_instance_ip)
+        print(array_instance_id)
+
+    # deregister any previous instances from the target group
+    for i in range(num_of_shards):
+        key_tg = "tg_s" + str(i)
+        array_target_group = parse_network_config(key_tg)
+        print(array_target_group)
+
+
 
 def main():
     """  """
