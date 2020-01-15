e53c1b84315f65e91e8c86fbf7cdad5c1cda3a71
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-01-14 20:58:11 -0800

    [tx-gen] Update devnet config

--- test-automation/tx-gen/devnet_example.py
+++ test-automation/tx-gen/devnet_example.py
@@ -20,11 +20,12 @@ tx_gen.set_config({
     "NUM_SRC_ACC": 16,  # The number of possible source accounts for all transactions
     "NUM_SNK_ACC": 1,  # The number of possible destination / sink accounts for all transaction
     "MAX_TXN_GEN_COUNT": None,  # The upper bound of the number generated transaction, regardless of if `stop` is called
-    "ONLY_CROSS_SHARD": True,  # If true, forces source and destination shards to be different
+    "ONLY_CROSS_SHARD": False,  # If true, forces source and destination shards to be different
+    "ENFORCE_NONCE": False,  # If true, will only generate transactions with a valid nonce
     "ESTIMATED_GAS_PER_TXN": 1e-3,  # The estimated gas, hardcoded
     "INIT_SRC_ACC_BAL_PER_SHARD": 1,  # The initial balance for EVERY source account
     "TXN_WAIT_TO_CONFIRM": 60,  # The timeout when a transaction is sent (only used in setup related functions)
-    "MAX_THREAD_COUNT": 8,  # Max thread is recommended to be less than your v-core count
+    "MAX_THREAD_COUNT": 16,  # Max thread is recommended to be less than your v-core count
     "ENDPOINTS": [  # Endpoints for all transaction, index i = shard i
         "https://api.s0.pga.hmny.io/",
         "https://api.s1.pga.hmny.io/",
@@ -69,26 +70,27 @@ if __name__ == "__main__":
         tx_gen.Loggers.transaction.logger.addHandler(logging.StreamHandler(sys.stdout))
         tx_gen.Loggers.report.logger.addHandler(logging.StreamHandler(sys.stdout))
 
-    log_writer_pool = ThreadPool(processes=1)
+    log_writer_pool = ThreadPool()
     log_writer_pool.apply_async(log_writer, (5,))
 
     config = tx_gen.get_config()
-    account_manager.load_accounts("./devnet_keys", passphrase="", fast_load=True)
+    account_manager.load_accounts("./faucet_key", passphrase="", fast_load=True)  # More keys = faster funding process.
     source_accounts = tx_gen.create_accounts(config["NUM_SRC_ACC"], "src_acc")
     sink_accounts = tx_gen.create_accounts(config["NUM_SNK_ACC"], "snk_acc")
     tx_gen.fund_accounts(source_accounts)
 
-    tx_gen_pool = ThreadPool(processes=1)
+    tx_gen_pool = ThreadPool()
     start_time = datetime.datetime.utcnow()
     tx_gen_pool.apply_async(lambda: tx_gen.start(source_accounts, sink_accounts))
-    time.sleep(15)
+    time.sleep(60)
     tx_gen.stop()
     end_time = datetime.datetime.utcnow()
-    tx_gen.return_balances(source_accounts)
-    tx_gen.return_balances(sink_accounts)
-    tx_gen.remove_accounts(source_accounts)
-    tx_gen.remove_accounts(sink_accounts)
-    time.sleep(35)
+    # tx_gen.return_balances(source_accounts)
+    # tx_gen.return_balances(sink_accounts)
+    # tx_gen.remove_accounts(source_accounts, backup=False)
+    # tx_gen.remove_accounts(sink_accounts, backup=False)
+    time.sleep(60)
+    print(tx_gen.Loggers.transaction.filename, start_time, end_time)
     report = analysis.verify_transactions(tx_gen.Loggers.transaction.filename, start_time, end_time)
     print(report)
     assert report["received-transaction-report"]["successful-transactions-total"] > 1
