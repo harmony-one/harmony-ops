cb149efddc0d94f5e480fcc95abaf5b8850b2383
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-01-20 17:49:38 -0800

    [tx-gen] Checkpoint b4 initial release to main-repo (9)

--- test-automation/tx-gen/harmony_transaction_generator/__init__.py
+++ test-automation/tx-gen/harmony_transaction_generator/__init__.py
@@ -1,3 +1,6 @@
+import sys
+import warnings
+
 from .common import (
     Loggers,
     set_config,
@@ -26,3 +29,21 @@ from .generator import (
     start,
     stop,
 )
+
+if sys.version_info.major < 3:
+    warnings.simplefilter("always", DeprecationWarning)
+    warnings.warn(
+        DeprecationWarning(
+            "`harmony-transaction-generator` does not support Python 2. Please use Python 3."
+        )
+    )
+    warnings.resetwarnings()
+
+if sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
+    warnings.simplefilter("always", ImportWarning)
+    warnings.warn(
+        ImportWarning(
+            "`harmony-transaction-generator` does not work on Windows or Cygwin."
+        )
+    )
+    warnings.resetwarnings()
