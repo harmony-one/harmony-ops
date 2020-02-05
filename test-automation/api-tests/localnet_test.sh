93e9d9202d41df218589fe6bf0f0fd8db0984b9b
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-02-05 10:16:37 -0800

    [api-test] Add wait option b4 test

--- test-automation/api-tests/localnet_test.sh
+++ test-automation/api-tests/localnet_test.sh
@@ -6,18 +6,21 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 delay=30
 iters=5
 timeout_dir=60
+wait=120
 
-while getopts ht:d:i: option; do
+while getopts hw:t:d:i: option; do
   case "${option}" in
 
+  w) wait=${OPTARG} ;;
   t) timeout_dir=${OPTARG} ;;
   d) delay=${OPTARG} ;;
   i) iters=${OPTARG} ;;
   h)
     echo "Options:"
     echo ""
-    echo "  Example: ./localnet_test.sh -t 60 -d 30 -i 5"
+    echo "  Example: ./localnet_test.sh -w 60 -t 60 -d 30 -i 5"
     echo ""
+    echo "    -w <int>  Delay (in seconds) before starting the test. Default is 120 seconds."
     echo "    -t <int>  Timeout before localnet is spun up. Default is 60 seconds."
     echo "    -d <int>  Cx delay (in seconds) between send and check for tests. Default is 30 seconds."
     echo "    -i <int>  Max number of iterations before success. Default is 5."
@@ -55,7 +58,10 @@ function waitBoot() {
 
 timeout "$timeout_dir" cat <( tryConnect )
 timeout "$timeout_dir" cat <( waitBoot )
-sleep 15  # Grace b4 starting
+
+echo "Sleeping ${wait} seconds to generate some funds..."
+sleep $wait
+
 # TODO: Enable staking tests...
 python3 -u ${DIR}/test.py --test_dir=./tests/no-explorer/ --rpc_endpoint_src="http://localhost:9500/" \
   --rpc_endpoint_dst="http://localhost:9501/" --delay=${delay} --iterations=${iters} --cli_passphrase='' \
