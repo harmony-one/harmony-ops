f43c081cce7c8e60453e9751683858eaebce4a6b
Janet Liang <janet@harmony.one>
2020-02-26 15:23:47 -0800

    [mini-explorer] Explorer-like for load testing stats without launching real explorer

---
+++ devops/mini_explorer/mini_explorer.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+# Run command:
+# python3 -u mini_explorer.py [params] 2&>1 | tee out.log
+
+import argparse
+import json, os
+import datetime, time
+import requests
+from collections import defaultdict
+
+endpoint = "https://api.s%d.%s.hmny.io/"
+
+def latestBlock() -> dict:
+    return {"id": "1", "jsonrpc": "2.0",
+            "method": "hmy_latestHeader",
+            "params": ["latest"]}
+
+def blockByHash(hash) -> dict:
+    return {"id": "1", "jsonrpc": "2.0",
+            "method": "hmy_getBlockByHash",
+            "params": [hash, True]}
+
+def request(endpoint, request, output = False) -> str:
+    # Send request
+    r = requests.get(endpoint, headers = {'Content-Type':'application/json; charset=utf8'}, data = request)
+    # Check for invalid status code
+    if r.status_code != 200:
+        print("Error: Return status code %s" % r.status_code)
+        return None
+
+    # Check for valid JSON format return
+    try:
+        r.json()
+    except ValueError:
+        print("Error: Unable to read JSON reply")
+        return None
+
+    return r.json()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--shards', default = 2, type = int, help = 'Number of shards')
+    parser.add_argument('--network', default = 't', help = 'Short network name, for endpoints')
+    parser.add_argument('--sleep', default = 8, type = int, help = 'Sleep timer')
+
+    args = parser.parse_args()
+
+    try:
+        while True:
+            for x in range(args.shards):
+                result = request(endpoint % (x, args.network), json.dumps(latestBlock()))
+                if result != None:
+                    hash = result["result"]["blockHash"]
+                    next = request(endpoint % (x, args.network), json.dumps(blockByHash(hash)))
+                    if next != None:
+                        sx = defaultdict(int)
+                        sx["total"] = len(next["result"]["stakingTransactions"])
+                        for t in next["result"]["stakingTransactions"]:
+                            sx[t["type"]] += 1
+                        t = json.dumps({"shard": x,
+                                        "leader": result["result"]["leader"],
+                                        "timestamp": result["result"]["timestamp"],
+                                        "block": result["result"]["blockNumber"],
+                                        "epoch": result["result"]["epoch"],
+                                        "gas": int(next["result"]["gasUsed"], 0),
+                                        "size": int(next["result"]["size"], 0),
+                                        "transactions": len(next["result"]["transactions"]),
+                                        "staking": sx})
+                        print(t)
+            time.sleep(args.sleep)
+    except KeyboardInterrupt:
+        pass
