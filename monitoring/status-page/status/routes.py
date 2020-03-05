4dd4aa2297be84f75407456781a7d04069bebc35
Janet Liang <janet@harmony.one>
2020-03-04 18:45:02 -0800

    [status-page] Add links to Block Explorer & Staking Dashboard 	      Include API endpoints for each shard & network

--- monitoring/status-page/status/routes.py
+++ monitoring/status-page/status/routes.py
@@ -3,24 +3,33 @@ import json
 import requests
 from flask import render_template
 from status import app
+from collections import namedtuple
+
+NetworkInfo = namedtuple('NetworkInfo', ['name', 'watchdog', 'explorer', 'staking', 'endpoint'])
 
 watchdog = 'http://watchdog.hmny.io/status-%s'
+endpoint = 'https://api.s%s.%s.hmny.io'
 
 @app.route('/status')
 def status():
 
     with open('networks.txt', 'r') as f:
-        network_list = [x.strip().split(',') for x in f]
+        network_list = [NetworkInfo(*[y.strip() for y in x.strip().split(',')]) for x in f if not x[0] == '#']
 
     statuses = {}
     for n in network_list:
-        r = requests.get(watchdog % n[1], auth=('harmony', 'harmony.one'))
-        statuses[n[0]] = {}
+        r = requests.get(watchdog % n.watchdog, auth=('harmony', 'harmony.one'))
+        statuses[n.name] = {}
+        statuses[n.name]['block'] = {}
         for result in r.json():
-            statuses[n[0]][result["shard-id"]] = result
+            statuses[n.name]['block'][result['shard-id']] = result
+            statuses[n.name]['block'][result['shard-id']]['endpoint'] = endpoint % (result['shard-id'], n.endpoint)
+        statuses[n.name]['explorer-link'] = n.explorer
+        statuses[n.name]['staking-link'] = n.staking
 
+    # Sort output by ShardID
     for net in statuses.keys():
-        for k in statuses[net]:
-            statuses[net] = {key: value for key, value in sorted(statuses[net].items(), key = lambda item: item[1]["shard-id"])}
+        for k in statuses[net]['block']:
+            statuses[net]['block'] = {key: value for key, value in sorted(statuses[net]['block'].items(), key = lambda item: item[1]['shard-id'])}
 
     return render_template('status.html.j2', data = statuses)
