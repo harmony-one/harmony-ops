54cb2587b903b53e6b82623530e9aaa38483ed75
Janet Liang <janet@harmony.one>
2020-03-18 18:23:32 -0700

    [status-page] Thread requests to decrease loading time

--- monitoring/status-page/status/routes.py
+++ monitoring/status-page/status/routes.py
@@ -1,14 +1,21 @@
 import argparse
 import json
 import requests
-from flask import render_template
-from status import app
 from collections import namedtuple
+from flask import render_template
+from multiprocessing.pool import Pool
+from status import app
+from threading import Lock
 
 NetworkInfo = namedtuple('NetworkInfo', ['name', 'watchdog', 'explorer', 'staking', 'endpoint'])
 
 watchdog = 'http://watchdog.hmny.io/status-%s'
 endpoint = 'https://api.s%s.%s.hmny.io'
+
+statuses = {}
+pool = Pool()
+threads = []
+lock = Lock()
 
 @app.route('/status')
 def status():
@@ -16,33 +23,20 @@ def status():
     with open('networks.txt', 'r') as f:
         network_list = [NetworkInfo(*[y.strip() for y in x.strip().split(',')]) for x in f if not x[0] == '#']
 
-    statuses = {}
     for n in network_list:
+        lock.acquire()
         statuses[n.name] = {}
         statuses[n.name]['block'] = {}
-        try:
-            r = requests.get(watchdog % n.watchdog, auth=('harmony', 'harmony.one'))
-        except requests.exceptions.ConnectionError:
-            r = None
-        if r is not None:
-            result = r.json()
-            try:
-                for item in result['shard-status']:
-                    statuses[n.name]['block'][item['shard-id']] = item
-                    e = endpoint % (item['shard-id'], n.endpoint)
-                    statuses[n.name]['block'][item['shard-id']]['endpoint'] = e
-                    try:
-                        s = requests.get(e)
-                        statuses[n.name]['block'][item['shard-id']]['endpoint-status'] = True
-                    except requests.exceptions.ConnectionError:
-                        statuses[n.name]['block'][item['shard-id']]['endpoint-status'] = False
-            except json.decoder.JSONDecodeError:
-                pass
-            #statuses[n.name]['used-seats'] = result['used-seats']
-            #statuses[n.name]['avail-seats'] = result['avail-seats']
-            statuses[n.name]['commit-version'] = result['commit-version']
-            statuses[n.name]['explorer-link'] = n.explorer
-            statuses[n.name]['staking-link'] = n.staking
+        statuses[n.name]['explorer-link'] = n.explorer
+        statuses[n.name]['staking-link'] = n.staking
+        lock.release()
+        query_watchdog(n.name, n.watchdog, n.endpoint)
+        #threads.append(pool.apply_async(query_watchdog, (n.name, n.watchdog, n.endpoint)))
+
+    for t in threads:
+        t.get()
+
+    pool.close()
 
     # Sort output by ShardID
     for net in statuses.keys():
@@ -50,3 +44,38 @@ def status():
             statuses[net]['block'] = {key: value for key, value in sorted(statuses[net]['block'].items(), key = lambda item: item[1]['shard-id'])}
 
     return render_template('status.html.j2', data = statuses)
+
+def query_watchdog(network_name, network_watchdog, network_endpoint):
+    try:
+        r = requests.get(watchdog % network_watchdog, auth=('harmony', 'harmony.one'))
+    except requests.exceptions.ConnectionError:
+        return
+
+    try:
+        result = r.json()
+        for i in result['shard-status']:
+            id = item['shard-id']
+            e = endpoint % (item['shard-id'], network_endpoint)
+            lock.acquire()
+            statuses[network_name]['block'][id] = item
+            statuses[network_name]['block'][id]['endpoint'] = e
+            lock.release()
+            #threads.append(pool.apply_async(check_endpoint, (e, id, network_name)))
+            check_endpoint(e, id, network_name)
+        lock.acquire()
+        statuses[network_name]['commit-version'] = result['commit-version']
+        statuses[network_name]['used-seats'] = result['used-seats']
+        statuses[network_name]['avail-seats'] = result['avail-seats']
+        lock.release()
+    except json.decoder.JSONDecodeError:
+        pass
+
+def check_endpoint(endpoint, shard_id, network_name):
+    try:
+        s = requests.get(endpoint)
+        avail = True
+    except requests.exceptions.ConnectionError:
+        avail = False
+    lock.acquire()
+    statuses[network_name]['block'][shard_id]['endpoint-status'] = avail
+    lock.release()
