b600bbcd5f613567ee88dc9239817366b4cbd0b6
janet-harmony <56005637+janet-harmony@users.noreply.github.com>
2020-03-24 01:08:00 -0700

    [status-page] Fix slow loading times (#496)
    [status-page] Thread requests to decrease loading time
    [status-page] Fix threading issues for faster loading
    [status-page] Move version next to network name (harmony-#2491)
    [status-page] Show total number of committee seats & used seats

--- monitoring/status-page/status/routes.py
+++ monitoring/status-page/status/routes.py
@@ -1,48 +1,66 @@
 import argparse
 import json
+import queue
 import requests
+from collections import namedtuple
 from flask import render_template
 from status import app
-from collections import namedtuple
+from threading import Thread
 
-NetworkInfo = namedtuple('NetworkInfo', ['name', 'watchdog', 'explorer', 'staking', 'endpoint'])
+NetworkInfo = namedtuple('NetworkInfo', ['watchdog', 'explorer', 'staking', 'endpoint'])
 
 watchdog = 'http://watchdog.hmny.io/status-%s'
 endpoint = 'https://api.s%s.%s.hmny.io'
+
+statuses = {}
+q = queue.Queue(maxsize = 0)
+
 
 @app.route('/status')
 def status():
 
     with open('networks.txt', 'r') as f:
-        network_list = [NetworkInfo(*[y.strip() for y in x.strip().split(',')]) for x in f if not x[0] == '#']
+        network_list = {x.strip().split(',')[0].strip(): NetworkInfo(*[y.strip() for y in x.strip().split(',')][1:]) for x in f if not x[0] == '#'}
 
-    statuses = {}
-    for n in network_list:
-        statuses[n.name] = {}
-        statuses[n.name]['block'] = {}
-        try:
-            r = requests.get(watchdog % n.watchdog, auth=('harmony', 'harmony.one'))
-        except requests.exceptions.ConnectionError:
-            r = None
-        if r is not None:
-            result = r.json()
-            try:
-                for item in result['shard-status']:
-                    statuses[n.name]['block'][item['shard-id']] = item
-                    e = endpoint % (item['shard-id'], n.endpoint)
-                    statuses[n.name]['block'][item['shard-id']]['endpoint'] = e
-                    try:
-                        s = requests.get(e)
-                        statuses[n.name]['block'][item['shard-id']]['endpoint-status'] = True
-                    except requests.exceptions.ConnectionError:
-                        statuses[n.name]['block'][item['shard-id']]['endpoint-status'] = False
-            except json.decoder.JSONDecodeError:
-                pass
-            #statuses[n.name]['used-seats'] = result['used-seats']
-            #statuses[n.name]['avail-seats'] = result['avail-seats']
-            statuses[n.name]['commit-version'] = result['commit-version']
-            statuses[n.name]['explorer-link'] = n.explorer
-            statuses[n.name]['staking-link'] = n.staking
+    watchdog_threads = []
+    for n in network_list.keys():
+        statuses[n] = {}
+        statuses[n]['block'] = {}
+        statuses[n]['explorer-link'] = network_list[n].explorer
+        statuses[n]['staking-link'] = network_list[n].staking
+        watchdog_threads.append(Thread(target = query_watchdog, args = (n, network_list[n].watchdog, q)))
+
+    for w in watchdog_threads:
+        w.start()
+
+    for w in watchdog_threads:
+        w.join()
+
+    endpoint_threads = []
+    while not q.empty():
+        network_name, result = q.get()
+        for item in result['shard-status']:
+            id = item['shard-id']
+            e = endpoint % (item['shard-id'], network_list[network_name].endpoint)
+            statuses[network_name]['block'][id] = item
+            statuses[network_name]['block'][id]['endpoint'] = e
+            endpoint_threads.append(Thread(target = check_endpoint, args = (e, id, network_name, q)))
+        if len(result['commit-version']) == 1:
+            statuses[network_name]['commit-version'] = result['commit-version'][0]
+        else:
+            statuses[network_name]['commit-version'] = ", ".join(result['commit-version'])
+        statuses[network_name]['used-seats'] = result['used-seats']
+        statuses[network_name]['avail-seats'] = result['avail-seats']
+
+    for e in endpoint_threads:
+        e.start()
+
+    for e in endpoint_threads:
+        e.join()
+
+    while not q.empty():
+        network_name, shard_id, avail = q.get()
+        statuses[network_name]['block'][shard_id]['endpoint-status'] = avail
 
     # Sort output by ShardID
     for net in statuses.keys():
@@ -50,3 +68,23 @@ def status():
             statuses[net]['block'] = {key: value for key, value in sorted(statuses[net]['block'].items(), key = lambda item: item[1]['shard-id'])}
 
     return render_template('status.html.j2', data = statuses)
+
+def query_watchdog(network_name, network_watchdog, q):
+    try:
+        r = requests.get(watchdog % network_watchdog, auth=('harmony', 'harmony.one'))
+    except requests.exceptions.ConnectionError:
+        return
+
+    try:
+        result = r.json()
+        q.put((network_name, result))
+    except json.decoder.JSONDecodeError:
+        pass
+
+def check_endpoint(endpoint, shard_id, network_name, q):
+    try:
+        s = requests.get(endpoint)
+        avail = True
+    except requests.exceptions.ConnectionError:
+        avail = False
+    q.put((network_name, shard_id, avail))
