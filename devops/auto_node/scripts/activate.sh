d24086eb234c5f8d4fbc2a95f245ec4a444598cc
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-04-03 02:20:40 -0700

    [auto_node] Improve flow for general usage
    * Remove need for wallet private key, use shared CLI keystore.
    * Default options to OSTN.
    * Add validator config JSON file option.

--- devops/auto_node/scripts/activate.sh
+++ devops/auto_node/scripts/activate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./bin/hmy staking edit-validator --validator-addr $(cat /.val_address) --active true -n $(cat /.beacon_endpoint)
+./bin/hmy staking edit-validator --validator-addr $(cat /.val_address) --active true -n $(cat /.beacon_endpoint) --passphrase-file /.wallet_passphrase
