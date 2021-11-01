2700b18e8859142f05f98345fd6097d47316ff80
Nita Neou <soph@harmony.one>
2021-11-01 05:06:00 +0000

    [grafana-prometheus] add btc-bridge support

--- grafana/update_db_config.py
+++ grafana/update_db_config.py
@@ -30,7 +30,7 @@ dict_grafana_folder_mode = {
     "testnet": 105
 }
 do_node_ips = []
-service_list = ['bridge', 'graph', 'multisig', 'contractverify', 'explorerv2']
+service_list = ['bridge', 'graph', 'multisig', 'contractverify', 'explorerv2', 'btc-bridge']
 
 
 def shcmd(cmd, ignore_error=False):
